4198b2ea4b7ff2be6d41e03238ae9ea859af21cf
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-07-23 12:18:42 +0200

    feat: align elements to world

--- src/button.gd
+++ src/button.gd
@@ -8,10 +8,10 @@ const COLOR_DOWN := Color(0.0, 0.704, 0.152, 1.0)
 var pressed := false
 var overlaps := 0
 
-@onready var visual: Polygon2D = $Polygon2D
+@onready var visual: Sprite2D = $Sprite2D
 
 func _ready() -> void:
-	visual.color = COLOR_UP
+	visual.modulate = COLOR_UP
 
 func _on_entered(_node: Node2D) -> void:
 	overlaps += 1
@@ -25,5 +25,5 @@ func set_pressed(value: bool) -> void:
 	if pressed == value: return
 
 	pressed = value
-	visual.color = COLOR_DOWN if value else COLOR_UP
+	visual.modulate = COLOR_DOWN if value else COLOR_UP
 	changed.emit(value)
