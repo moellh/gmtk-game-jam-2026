6ed59d2cd0093152a267dcdf2295a8bd07c9cb9d
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-07-23 08:46:12 +0200

    feat: ghost early exit and reset

--- src/game.gd
+++ src/game.gd
@@ -7,8 +7,15 @@ const ROUND_TIME := 10.0
 var timer := ROUND_TIME
 
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("clear"): clear()
+
 	timer -= delta
-	if timer <= 0.0: next_round()
+	if timer <= 0.0 or Input.is_action_just_pressed("restart"): next_round()
+
+func clear() -> void:
+	timer = ROUND_TIME
+	get_tree().call_group("ghosts", "queue_free")
+	player.reset()
 
 func next_round() -> void:
 	timer = ROUND_TIME
