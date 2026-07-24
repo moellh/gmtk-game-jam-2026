e2fa961caefed965f5dba323c9a33519145969c6
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-07-24 06:39:04 +0200

    fix: position based

--- src/ghost.gd
+++ src/ghost.gd
@@ -1,34 +1,20 @@
-extends Figure
+extends AnimatedSprite2D
 
-var recording: Array[Dictionary] = []
+var recording: Array = []
 var playback := 0
-var spawn: Vector2
-var live_player: PhysicsBody2D
 
-func setup(
-	actions: Array[Dictionary],
-	source_collision: CollisionShape2D,
-	player: PhysicsBody2D,
-) -> void:
-	recording.assign(actions)
-	live_player = player
-
-	var collision_shape := get_node("CollisionShape2D") as CollisionShape2D
-	collision_shape.position = source_collision.position
-	collision_shape.shape = source_collision.shape
-
-func _ready() -> void:
-	spawn = global_position
-	add_collision_exception_with(live_player)
-	reset_figure(spawn)
+func setup(rec: Array) -> void:
+	recording = rec
 
 func restart() -> void:
 	playback = 0
-	reset_figure(spawn)
 
-func _physics_process(delta: float) -> void:
-	if playback >= recording.size():
-		return
+func _physics_process(_delta: float) -> void:
+	if playback >= recording.size(): return # Freeze on end
 
-	apply_action(recording[playback], delta)
+	var f: Dictionary = recording[playback]
+	global_position = f.pos
+	flip_h = f.flip
+	play(f.anim)
+
 	playback += 1
--- src/player.gd
+++ src/player.gd
@@ -1,36 +1,68 @@
-extends Figure
+extends CharacterBody2D
+
+const SPEED := 200.0
+const JUMP_VELOCITY := -300.0
+const ACCEL := 2000.0
+const FRICTION := 2500.0
+const LEDGE_TIME := 0.1
+const JUMP_BUFFER := 0.1
 
 const GHOST_SCENE := preload("res://src/ghost.tscn")
 
+var ledge_timer := 0.0
+var buffer_timer := 0.0
+
 var spawn: Vector2
-var recording_start: Vector2
 var recording: Array[Dictionary] = []
 
-@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	add_to_group(&"player")
 	spawn = global_position
-	recording_start = spawn
 
 func _physics_process(delta: float) -> void:
-	var action: Dictionary = {
-		MOVE_AXIS: Input.get_axis("move_left", "move_right"),
-		JUMP_PRESSED: Input.is_action_just_pressed("jump"),
-		JUMP_RELEASED: Input.is_action_just_released("jump"),
-	}
-	
-	apply_action(action, delta)
-	recording.append(action)
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	ledge_timer = LEDGE_TIME if is_on_floor() else ledge_timer - delta
+	buffer_timer = JUMP_BUFFER if Input.is_action_just_pressed("jump") else buffer_timer - delta
+	if buffer_timer > 0.0 and ledge_timer > 0.0:
+		velocity.y = JUMP_VELOCITY
+		buffer_timer = 0.0
+		ledge_timer = 0.0
+
+	if Input.is_action_just_released("jump") and velocity.y < 0.0:
+		velocity.y *= 0.4
+
+	var move_axis := Input.get_axis("move_left", "move_right")
+	if move_axis:
+		velocity.x = move_toward(velocity.x, move_axis * SPEED, 1.5 * ACCEL * delta)
+		sprite.flip_h = move_axis < 0.0
+	else:
+		velocity.x = move_toward(velocity.x, 0.0, FRICTION * delta)
+
+	move_and_slide()
+	update_animation(move_axis)
+
+	recording.append({ pos = global_position, flip = sprite.flip_h, anim = sprite.animation })
+
+func update_animation(move_axis: float) -> void:
+	if not is_on_floor():
+		sprite.play(&"jump")
+	elif move_axis != 0.0:
+		sprite.play(&"run")
+	else:
+		sprite.play(&"idle")
 
 func spawn_ghost() -> Node:
 	var ghost := GHOST_SCENE.instantiate()
-	ghost.setup(recording, collision_shape, self)
-	ghost.global_position = recording_start
-	ghost.add_to_group("ghosts")
+	ghost.setup(recording)
+	ghost.global_position = spawn
+	ghost.add_to_group(&"ghosts")
 	return ghost
 
 func reset() -> void:
 	recording = []
-	recording_start = spawn
-	reset_figure(recording_start)
+	velocity = Vector2.ZERO
+	global_position = spawn
