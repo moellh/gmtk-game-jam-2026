108ef7333884800d65ad0f4fd965417261c02028
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-07-24 18:13:51 +0200

    feat: time glitch effect

--- src/decorations/round_timer.gd
+++ src/decorations/round_timer.gd
@@ -1,12 +1,20 @@
 class_name RoundTimer
 extends Label
 
+const DANGER_COLOR := Color(1.0, 0.25, 0.25)
+
 @export var round_time := 10.0
+@export var danger_curve := 5.0
 
 var _time_left := 0.0
 
+@onready var _base_position := position
+
 func _ready() -> void:
 	reset()
+
+func _exit_tree() -> void:
+	Glitch.set_danger(0.0)
 
 func reset() -> void:
 	_time_left = round_time
@@ -20,6 +28,11 @@ func is_expired() -> bool:
 	return _time_left <= 0.0
 
 func _refresh() -> void:
+	var danger := pow(1.0 - _time_left / round_time, danger_curve)
+	Glitch.set_danger(danger)
+
 	var tenths := roundi(_time_left * 10.0)
 	var seconds := mini(floori(tenths * 0.1), 99)
 	text = "%d.%d" % [seconds, tenths % 10]
+	position = _base_position + Vector2(randf_range(-1.0, 1.0), randf_range(-1.0, 1.0)) * danger * 2.0
+	self_modulate = Color.WHITE.lerp(DANGER_COLOR, danger)
--- src/effects/glitch.gd
+++ src/effects/glitch.gd
@@ -1,16 +1,29 @@
 extends CanvasLayer
 
-const DURATION := 0.25
+const BURST_TIME := 0.25
 
 @onready var rect: ColorRect = $Rect
 @onready var _mat: ShaderMaterial = rect.material
 
+var _burst := 0.0
+var _danger := 0.0
 var _tween: Tween
 
 func play() -> void:
 	if _tween: _tween.kill()
-	rect.visible = true
-	_mat.set_shader_parameter("strength", 1.0)
 	_tween = create_tween()
-	_tween.tween_property(_mat, "shader_parameter/strength", 0.0, DURATION)
-	_tween.tween_callback(func() -> void: rect.visible = false)
+	_tween.tween_method(_set_burst, 1.0, 0.0, BURST_TIME)
+
+func set_danger(value: float) -> void:
+	_danger = clampf(value, 0.0, 1.0)
+	_apply()
+
+func _set_burst(value: float) -> void:
+	_burst = value
+	_apply()
+
+func _apply() -> void:
+	var glitch := maxf(_burst, _danger)
+	rect.visible = glitch > 0.001
+	_mat.set_shader_parameter("strength", glitch)
+	_mat.set_shader_parameter("red", _danger)
