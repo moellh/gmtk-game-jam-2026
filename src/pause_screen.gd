5e2480521c80866f089a91e65e7e0ef7b42ed8f9
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-07-24 13:51:10 +0200

    fix: pause for the rest of the levels

--- src/pause_screen.gd
+++ src/pause_screen.gd
@@ -2,21 +2,17 @@ extends CanvasLayer
 
 @onready var screen: Control = $Screen
 
-
 func _unhandled_input(event: InputEvent) -> void:
-	if not event.is_action_pressed("ui_cancel"):
-		return
+	if not event.is_action_pressed("ui_cancel"): return
 
 	if screen.visible:
 		get_viewport().set_input_as_handled()
 		screen.hide()
 		get_tree().paused = false
-	elif not get_tree().paused and get_parent().can_pause():
+	elif not get_tree().paused:
 		get_viewport().set_input_as_handled()
 		screen.show()
 		get_tree().paused = true
 
-
 func _exit_tree() -> void:
-	if screen.visible:
-		get_tree().paused = false
+	if screen.visible: get_tree().paused = false
