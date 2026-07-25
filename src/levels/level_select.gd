7b704cf102abf5edb117cdca30fe05ee55871e2d
floppyMike <36990990+floppyMike@users.noreply.github.com>
2026-07-25 14:57:54 +0200

    feat: level selection menu

--- src/levels/level_select.gd
+++ src/levels/level_select.gd
@@ -2,16 +2,47 @@ extends Control
 
 @export var levels: Array[LevelInfo] = []
 
-@onready var list: VBoxContainer = $Center/VBox/Levels
+var _index := 0
+var _thumbs := {}
+
+@onready var _name_label: Label = $Window/VBox/Name
+@onready var _preview: TextureRect = $Window/VBox/Row/PreviewFrame/Preview
+@onready var _capture: SubViewport = $Capture
 
 func _ready() -> void:
-	for i in levels.size():
-		var info := levels[i]
-		var button := Button.new()
+	_refresh()
 
-		button.text = info.name
-		button.pressed.connect(get_tree().change_scene_to_packed.bind(info.scene))
-		button.mouse_entered.connect(button.grab_focus)
-		list.add_child(button)
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_left"): _step(-1)
+	elif event.is_action_pressed("ui_right"): _step(1)
+	elif event.is_action_pressed("ui_accept") and not levels.is_empty():
+		get_tree().change_scene_to_packed(levels[_index].scene)
 
-		if i == 0: button.grab_focus()
+func _step(dir: int) -> void:
+	if levels.is_empty(): return
+	_index = wrapi(_index + dir, 0, levels.size())
+	_refresh()
+
+func _refresh() -> void:
+	if levels.is_empty(): return
+	var index := _index
+	_name_label.text = levels[index].name
+	var texture := await _thumbnail(index)
+	if index == _index: _preview.texture = texture
+
+func _thumbnail(index: int) -> Texture2D:
+	if _thumbs.has(index): return _thumbs[index]
+
+	var instance := levels[index].scene.instantiate()
+	_capture.add_child(instance)
+	await get_tree().process_frame
+	await RenderingServer.frame_post_draw
+
+	var texture := ImageTexture.create_from_image(_capture.get_texture().get_image())
+	instance.queue_free()
+	
+	# HACK: the instantiated scene pauses the root (also menu) => unpause
+	get_tree().paused = false
+
+	_thumbs[index] = texture
+	return texture
