4ed8554ea6a81315711d1dd63df4bf72701533d8
Lian Jahns <lian.j@gmx.de>
2026-07-26 15:19:42 +0200

    Added mode-tooltips

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -2,10 +2,13 @@ extends Control
 
 @onready var buttons: VBoxContainer = %Buttons
 
+const NORMAL_INFO := "Ghosts are harmless."
+const HAUNTING_INFO := "Touching your own ghost kills you instantly."
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%ModeToggle.button_pressed = GameMode.haunting_enabled
-	%ModeToggle.text = "Haunting Mode" if GameMode.haunting_enabled else "Normal Mode"
+	_update_mode_ui()
 	
 	for button in buttons.get_children():
 		if not button is Button: continue
@@ -18,7 +21,11 @@ func _level_selection() -> void:
 
 func _mode_toggle(pressed: bool) -> void:
 	GameMode.haunting_enabled = pressed
-	%ModeToggle.text = "Haunting Mode" if pressed else "Normal Mode"
+	_update_mode_ui()
+
+func _update_mode_ui() -> void:
+	%ModeToggle.text = "Haunting Mode" if GameMode.haunting_enabled else "Normal Mode"
+	%ModeInfo.text = HAUNTING_INFO if GameMode.haunting_enabled else NORMAL_INFO
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
